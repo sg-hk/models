2b49588a11c5c52af0cfd8e59c26b657700384e1
gitea <mark_down@tuta.com>
2025-05-17 11:30:34 +0800

    initial commit

---
+++ BSM/pricer.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>    /* std::erf, std::exp, std::log, std::sqrt */
+
+double norm_cdf(double x);
+double bs_call(double S, double K, double σ, double r, double T);
+double bs_put(double S, double K, double σ, double r, double T);
+
+int 
+main(int argc, char* argv[]) 
+{
+    /* ARG PARSING */
+
+    if (argc != 7) {
+        std::cerr << "Usage: " << argv[0]
+                  << " <C|P> S K σ r T\n";
+        return 1;
+    }
+    char type = argv[1][0];
+    double S     = std::stod(argv[2]);
+    double K     = std::stod(argv[3]);
+    double σ     = std::stod(argv[4]);
+    double r     = std::stod(argv[5]);
+    double T     = std::stod(argv[6]);
+
+    if (S <= 0 || K <= 0 || σ <= 0 || T <= 0) {
+        std::cerr << "All numeric inputs must be positive\n";
+        return 1;
+    }
+
+    double price;
+    if (type == 'C' || type == 'c') {
+        price = bs_call(S, K, σ, r, T);
+    } else if (type == 'P' || type == 'p') {
+        price = bs_put(S, K, σ, r, T);
+    } else {
+        std::cerr << "First arg must be C (call) or P (put)\n";
+        return 1;
+    }
+
+    std::cout << "Price: " << price << "\n";
+    return 0;
+}
+
+/* cumulative normal distribution via std::erf */
+double 
+norm_cdf(double x) 
+{
+    return 0.5 * (1.0 + std::erf(x / std::sqrt(2.0)));
+}
+
+double 
+bs_call(double S, double K, double σ, double r, double T) 
+{
+    double d1 = (std::log(S/K) + (r + 0.5*σ*σ)*T) / (σ*std::sqrt(T));
+    double d2 = d1 - σ*std::sqrt(T);
+    return S*norm_cdf(d1) - K*std::exp(-r*T)*norm_cdf(d2);
+}
+
+double 
+bs_put(double S, double K, double σ, double r, double T) 
+{
+    double d1 = (std::log(S/K) + (r + 0.5*σ*σ)*T) / (σ*std::sqrt(T));
+    double d2 = d1 - σ*std::sqrt(T);
+    return K*std::exp(-r*T)*norm_cdf(-d2) - S*norm_cdf(-d1);
+}
